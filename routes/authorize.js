d5c07c7f5bd4607141fb4b598b95efea0d0b4135
josh.loehr <josh.loehr@emergencyreporting.com>
2019-06-30 12:52:18 -0700

    Begin implicit flow and param validation

--- routes/authorize.js
+++ routes/authorize.js
@@ -1,14 +1,48 @@
 const { errorWithCode } = require('../util');
 
+const verifyRequiredParam = (param, req) => {
+    if (!req.body[param]) {
+        throw errorWithCode(`${param} is a required parameter`, 400);
+    }
+};
+
+// For implicit flow, response_type is required to include "id_token".
+// In addition, "token" is also allowed; all others are ignored.
+const validateResponseType = response_type => {
+    const parsed = response_type.split(' ').reduce((response_types, type) => {
+        return { ...response_types, [type]: true };
+    }, {});
+
+    if (!parsed.id_token) {
+        throw errorWithCode(`Invalid response_type "${response_type}"`, 400);
+    }
+
+    return parsed;
+};
+
+// For implicit flow, redirect_uri must use https, with http://localhost:xxxx as an exception.
+const validateRedirectUri = redirect_uri => {
+    const regex = /http:\/\/localhost(:\d+)?(\/|$).*/g;
+    if (redirect_uri.startsWith('http:') && !redirect_uri.match(regex)) {
+        throw errorWithCode(`Invalid redirect_uri "${redirect_uri}" (may only use http scheme for localhost)`, 400);
+    }
+    if (!redirect_uri.startsWith('https')) {
+        throw errorWithCode(`Invalid redirect_uri "${redirect_uri}" (only localhost may use http)`, 400);
+    }
+    return redirect_uri;
+};
+
 module.exports = {
     authorize: dao => async (req, res, next) => {
         try {
-            const requiredArgs = ['login', 'password'];
-            requiredArgs.forEach(arg => {
-                if (!req.body[arg]) {
-                    throw errorWithCode(`${arg} is a required parameter`, 400);
-                }
-            });
+            const requiredParams = ['scope', 'response_type', 'client_id', 'redirect_uri'];
+            requiredParams.forEach(param => verifyRequiredParam(param, req));
+
+            const { scope, response_type, client_id, redirect_uri } = req.body;
+            const config = {};
+
+            config.response_type = validateResponseType(response_type);
+            config.redirect_uri = validateRedirectUri(redirect_uri);
 
             const { login, password } = req.body;
 
--- tests/routes/authorize.test.js
+++ tests/routes/authorize.test.js
@@ -17,26 +17,58 @@ afterEach(() => {
     sinon.restore();
 });
 
+const validRequestParams = {
+    scope: 'openid',
+    response_type: 'id_token token',
+    client_id: 'app123',
+    redirect_uri: 'http://localhost:3000'
+};
+
+const testRequiredParam = param => async () => {
+    req.body = { ...validRequestParams, [param]: null };
+
+    let next = sinon.spy();
+    await authorize(dao)(req, res, next);
+
+    expect(next.getCall(0).args[0].statusCode).to.equal(400);
+};
+
 describe('Authorize Route', () => {
-    describe('authorize() function', () => {
-        it('Should return 400 error when login is not provided', async () => {
-            req.body.password = 'password';
+    describe('Implicit Flow', () => {
+        ['scope', 'response_type', 'client_id', 'redirect_uri'].forEach(param => {
+            it(`Should return 400 error when ${param} is not provided`, testRequiredParam(param));
+        });
+        it('Should return 400 error when response_type is neither "id_token" nor "id_token token"', async () => {
+            req.body = { ...validRequestParams, response_type: 'code' };
 
             let next = sinon.spy();
             await authorize(dao)(req, res, next);
 
             expect(next.getCall(0).args[0].statusCode).to.equal(400);
         });
-        it('Should return 400 error when password is not provided', async () => {
-            req.body.login = 'login';
+        it('Should return 400 error when redirect_uri malformed"', async () => {
+            req.body = { ...validRequestParams, redirect_uri: 'asdf' };
 
             let next = sinon.spy();
             await authorize(dao)(req, res, next);
 
             expect(next.getCall(0).args[0].statusCode).to.equal(400);
+        });
+        it('Should return 400 error when redirect_uri uses http without localhost"', async () => {
+            req.body = { ...validRequestParams, redirect_uri: 'http://www.google.com' };
+
+            let next = sinon.spy();
+            await authorize(dao)(req, res, next);
+
+            expect(next.getCall(0).args[0].statusCode).to.equal(400);
+
+            req.body = { ...validRequestParams, redirect_uri: 'http://localhost.fakeout.com' };
+
+            await authorize(dao)(req, res, next);
+            expect(next.getCall(1).args[0].statusCode).to.equal(400);
         });
         it('Should return 404 error when login does not exist', async () => {
-            req.body = { login: 'login', password: 'password' };
+            req.body = validRequestParams;
 
             dao.checkLoginExists.returns(Promise.resolve(false));
 
@@ -46,7 +78,7 @@ describe('Authorize Route', () => {
             expect(next.getCall(0).args[0].statusCode).to.equal(404);
         });
         it('Should return 401 error when incorrect password provided', async () => {
-            req.body = { login: 'login', password: 'password' };
+            req.body = validRequestParams;
 
             dao.checkLoginExists.returns(Promise.resolve(true));
             dao.getUser.returns(Promise.resolve(null));
